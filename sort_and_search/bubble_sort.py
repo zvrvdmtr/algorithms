0c404bb67fabbae13527c5555ab24104b19bb29c
Дмитрий Зверев <zverev@Dmitrijs-MacBook-Pro.local>
2019-10-15 21:52:10 +0300

    Add bubble sort

---
+++ sort_and_search/bubble_sort.py
@@ -0,0 +1,14 @@
+# Simple implementation for bubble sort
+
+
+def bubble_sort(data):
+    for item in range(len(data)-1):
+        for i in range(1, len(data)):
+            if data[i-1] > data[i]:
+                x = data[i-1]
+                data[i-1] = data[i]
+                data[i] = x
+
+
+alist = [54,26,93,17,77,31,44,55,20]
+bubble_sort(alist)
