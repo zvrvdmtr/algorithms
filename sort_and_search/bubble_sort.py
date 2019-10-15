53efe397480147f7d040f0277192f41bfc9c0b83
Дмитрий Зверев <zverev@Dmitrijs-MacBook-Pro.local>
2019-10-15 22:34:30 +0300

    Add selection sort

--- sort_and_search/bubble_sort.py
+++ sort_and_search/bubble_sort.py
@@ -1,6 +1,5 @@
 # Simple implementation for bubble sort
 
-
 def bubble_sort(data):
     for item in range(len(data)-1):
         for i in range(1, len(data)):
@@ -10,5 +9,5 @@ def bubble_sort(data):
                 data[i] = x
 
 
-alist = [54,26,93,17,77,31,44,55,20]
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 bubble_sort(alist)
